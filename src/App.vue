4a0c9cde0027ad749eb9e6a0c03d13b72828aa81
Ethan_nguyen <phuthanh0301@gmail.com>
2025-02-23 15:40:31 -0600

    Form input bindings

--- src/App.vue
+++ src/App.vue
@@ -1,422 +1,109 @@
 <template>
-  <div class="shopping-cart">
-        <h1>{{username}}'s Shopping Cart</h1>
-        <div class="cart-container">
-            <div class="cart-list">
-                <div class="cart-list-item">
-                    <img :src= "shoppingCartItems[0].image" 
-                    :alt="shoppingCartItems[0].productName" 
-                    class="product-image">
-                    <div class="item-details-with-actions">
-                        <div class="item-details">
-                            <h2>{{ shoppingCartItems[0].productName }}</h2>
-                            <p class="price">{{shoppingCartItems[0].price}}</p>
-                            <p class="in-stock-status"> <i class="fa-solid fa-check"></i> In stock</p>
-                        </div>
-                        <div class="item-actions">
-                            <div class="quantity-selector">
-                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[0].id)">−</button>
-                                <input type="text" class="quantity-input" 
-                                :value="shoppingCartItems[0].quantity" aria-label="quantity" @blur="changeQuantity(shoppingCartItems[0].id, $event)">
-                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[0].id)">+</button>
-                            </div>
-                            <button class="remove-item">✕</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="cart-list-item">
-                    <img :src= "shoppingCartItems[1].image" 
-                    :alt="shoppingCartItems[1].productName"  class="product-image">
-                    <div class="item-details-with-actions">
-                        <div class="item-details">
-                            <h2>{{ shoppingCartItems[1].productName }}</h2>
-                            <p class="price">{{shoppingCartItems[1].price}}</p>
-                            <p class="in-stock-status"> <i class="fa-solid fa-check"></i> In stock</p>
-                        </div>
-                        <div class="item-actions">
-                            <div class="quantity-selector">
-                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[1].id)">−</button>
-                                <input type="text" class="quantity-input" 
-                                :value="shoppingCartItems[1].quantity" aria-label="quantity" @blur="changeQuantity(shoppingCartItems[1].id, $event)">
-                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[1].id)">+</button>
-                            </div>
-                            <button class="remove-item">✕</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="cart-list-item">
-                    <img :src= "shoppingCartItems[2].image"  
-                    :alt="shoppingCartItems[2].productName"  class="product-image">
-                    <div class="item-details-with-actions">
-                        <div class="item-details">
-                            <h2>{{ shoppingCartItems[2].productName }}</h2>
-                            <p class="price">{{shoppingCartItems[2].price}}</p>
-                            <p class="on-backorder-status"> <i class="fa-solid fa-hourglass-half"></i> On backorder</p>
-                        </div>
-                        <div class="item-actions">
-                            <div class="quantity-selector">
-                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[2].id)">−</button>
-                                <input type="text" class="quantity-input" 
-                                :value="shoppingCartItems[2].quantity" aria-label="quantity" @blur="changeQuantity(shoppingCartItems[2].id, $event)">
-                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[2].id)">+</button>
-                            </div>
-                            <button class="remove-item">✕</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="cart-list-item">
-                    <img :src= "shoppingCartItems[3].image"  
-                    :alt="shoppingCartItems[3].productName" class="product-image">
-                    <div class="item-details-with-actions">
-                        <div class="item-details">
-                            <h2>{{ shoppingCartItems[3].productName }}</h2>
-                            <p class="price">{{shoppingCartItems[3].price}}</p>
-                            <p class="in-stock-status"> <i class="fa-solid fa-check"></i> In stock</p>
-                        </div>
-                        <div class="item-actions">
-                            <div class="quantity-selector">
-                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[3].id)">−</button>
-                                <input type="text" class="quantity-input" 
-                                :value="shoppingCartItems[3].quantity" 
-                                aria-label="quantity" @blur="changeQuantity(shoppingCartItems[3].id, $event)">
-                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[3].id)">+</button>
-                            </div>
-                            <button class="remove-item">✕</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="cart-list-item">
-                    <img :src= "shoppingCartItems[4].image"  
-                    :akl ="shoppingCartItems[4].productName"  class="product-image">
-                    <div class="item-details-with-actions">
-                        <div class="item-details">
-                            <h2>{{ shoppingCartItems[4].productName }}</h2>
-                            <p class="price">{{shoppingCartItems[4].price}}</p>
-                            <p class="in-stock-status"> <i class="fa-solid fa-check"></i> In stock</p>
-                        </div>
-                        <div class="item-actions">
-                            <div class="quantity-selector">
-                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[4].id)">−</button>
-                                <input type="text" class="quantity-input" 
-                                :value="shoppingCartItems[4].quantity" aria-label="quantity" @blur="changeQuantity(shoppingCartItems[4].id, $event)">
-                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[4].id)">+</button>
-                            </div>
-                            <button class="remove-item">✕</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="order-summary">
-                <h2>Order summary</h2>
-                <button class="toggle-details-button" @click="hideDetails = !hideDetails">{{hideDetails ? 'Show Details': 'Hide Details'}}</button>
-                <div :class="{'hide-order-details': hideDetails}">
-                    <div class="summary-item">
-                        <span>Subtotal</span>
-                        <span>$13900</span>
-                    </div>
-                    <div class="summary-item">
-                        <span>Shipping estimate</span>
-                        <span>$100</span>
-                    </div>
-                    <div class="summary-item">
-                        <span>Tax estimate</span>
-                        <span>$1112</span>
-                    </div>
-                </div>
-                <div class="summary-total">
-                    <strong>Order total</strong>
-                    <strong>$15112</strong>
-                </div>
-                <button class="checkout-button">Checkout</button>
-            </div>
-        </div>
+  <h1>{{ message }}</h1>
+  <h1>Welcome, {{ formData.name }}</h1>
+  <form action="">
+    <div>
+      <span>Name:</span>
+      <span>
+        <!-- formData.name is synced after "change" event instead of "input" event. -->
+        <input type="text" v-model.lazy="formData.name" />
+      </span>
     </div>
+    <div>
+      <span>Gender:</span>
+      <span>
+        <input type="radio" id="male" value="M" v-model="formData.gender" />
+        <label for="male">M</label>
+        <input type="radio" id="female" value="F" v-model="formData.gender" />
+        <label for="female">F</label>
+      </span>
+    </div>
+    <div>
+      <span>Age:</span>
+      <span>
+        <!-- Input is automatically typecast as a number. -->
+        <input type="text" id="age" v-model.number="formData.age" />
+      </span>
+    </div>
+    <div>
+      <span>Hobbies:</span>
+      <!-- We can bind multiple checkboxes to the same array. -->
+      <input type="checkbox" value="basketball" v-model="formData.hobbies" />
+      <label for="basketball">Basketball</label>
+      <input type="checkbox" value="football" v-model="formData.hobbies" />
+      <label for="football">Football</label>
+      <input type="checkbox" value="js" v-model="formData.hobbies" />
+      <label for="js">JavaScript</label>
+    </div>
+    <div>
+      <span>Profession:</span>
+      <select v-model="formData.profession">
+        <option value="">Please select one</option>
+        <option value="1">Software Engineer</option>
+        <option value="2">Data Scientist</option>
+        <option value="3">Product Manager</option>
+        <option value="4">Professor</option>
+      </select>
+    </div>
+    <div>
+      <span>Description:</span>
+      <!-- Leading and trailing whitespace is removed automatically. -->
+      <textarea
+        v-model.trim="formData.desc"
+        @keyup.enter="submitForm"
+      ></textarea>
+    </div>
+    <div>
+      <input type="reset" value="Reset" @click="resetForm" />
+      <!-- Prevent the form from submitting through form action. -->
+      <input type="submit" value="Submit" @click.prevent="submitForm" />
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { computed, ref, watch } from 'vue'
+import { ref } from 'vue'
 
-let username = 'Harry'
-let shoppingCartItems = ref([
-  {
-    id: 1,
-    productName: 'Dragon Liver',
-    price: 1500,
-    isInStock: true,
-    quantity: 3,
-    image: 'src/assets/img/DragonLiver.png'
-  },
-  {
-    id: 2,
-    productName: 'Golden Snitch',
-    price: 600,
-    isInStock: true,
-    quantity: 2,
-    image: ' src/assets/img/GoldenSnitch.png'
-  },
-  {
-    id: 3,
-    productName: 'Unicorn Tail Hair',
-    price: 1200,
-    isInStock: false,
-    quantity: 1,
-    image: 'src/assets/img/UnicornTailHair.png'
-  },
-  {
-    id: 4,
-    productName: 'Wand',
-    price: 2000,
-    isInStock: true,
-    quantity: 1,
-    image: 'src/assets/img/Wand.jpg'
-  },
-  {
-    id: 5,
-    productName: 'Nimbus 2000',
-    price: 5000,
-    isInStock: true,
-    quantity: 1,
-    image: 'src/assets/img/Nimbus2000.jpg'
+let message = ref('Hello, v-model!')
+
+let formData = ref({
+  name: '',
+  gender: '',
+  age: '',
+  hobbies: [],
+  profession: '',
+  desc: ''
+})
+
+function resetForm() {
+  formData.value = {
+    name: '',
+    gender: '',
+    age: '',
+    hobbies: [],
+    profession: '',
+    desc: ''
   }
-])
-
-let hideDetails = ref(false)
-
-function decreaseOne(id) {
-  shoppingCartItems.value.some((item) => {
-    if (item.id == id && item.quantity != 0) {
-      item.quantity = item.quantity - 1
-    }
-  })
 }
 
-function increaseOne(id) {
-  shoppingCartItems.value.some((item) => {
-    if (item.id == id) {
-      item.quantity = item.quantity + 1
-    }
-  })
+function submitForm() {
+  console.log(JSON.stringify(formData.value))
+  // We can use axios or fetch() to submit data to the back end.
 }
-
-function changeQuantity(id, event){
-  shoppingCartItems.value.some(item => {
-    if(item.id == id){
-      item.quantity = parseInt(event.target.value)
-    }
-  })
-}
-
-function removeItem(id) {
-  // Step 1: find the index of the item to be deleted
-  let index = shoppingCartItems.value.findIndex((item) => {
-    return item.id == id
-  })
-  // Step 2: delete this item from the list
-  shoppingCartItems.value.splice(index, 1)
-}
-let subtotal = computed(() =>
-  shoppingCartItems.value.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  )
-)
-
-let shippingEstimate = computed(() => (subtotal.value > 10000 ? 100 : 50))
-
-let taxEstimate = computed(() => subtotal.value * 0.08)
-
-let total = computed(
-  () => subtotal.value + shippingEstimate.value + taxEstimate.value
-)
-
-// Automatically save to localStorage whenever any part of shoppingCartItems changes
-watch(
-  shoppingCartItems,
-  () => {
-    localStorage.setItem(
-      'hogwartsShoppingCart',
-      JSON.stringify(shoppingCartItems.value)
-    )
-  },
-  { deep: true }
-)
 </script>
 
 <style scoped>
-/* Styles for the shopping cart */
-.shopping-cart {
-  font-family: 'Arial', sans-serif;
-  background-color: #f8f8f8;
-  margin: 0;
-  padding: 0;
-}
-
-/* Styles for the cart title */
-h1 {
+form {
   padding: 20px;
-  max-width: 1200px;
-  margin: auto;
 }
 
-/* Styles for the cart list and order summary */
-.cart-container {
-  display: flex;
-  align-items: flex-start;
-  max-width: 1200px;
-  margin: auto;
+form div {
+  height: 40px;
+  line-height: 40px;
 }
 
-.cart-list {
-  flex-grow: 2;
-  margin-right: 20px;
-}
-
-.order-summary {
-  flex-basis: 300px;
-  background-color: #f9fafb;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-/* Styles for the cart list item */
-.cart-list-item {
-  display: flex;
-  align-items: center;
-  background-color: #fff;
-  margin-bottom: 10px;
-  border-radius: 8px;
-  padding: 10px;
-}
-
-.product-image {
+form div span {
+  display: inline-block;
   width: 100px;
-  margin-right: 10px;
-}
-
-.item-details-with-actions {
-  display: flex;
-  flex-grow: 1;
-  margin-left: 10px;
-}
-
-.item-details {
-  flex-grow: 1;
-}
-
-.item-actions {
-  display: flex;
-  align-items: center;
-}
-
-.price {
-  font-size: 0.9em;
-  color: #666;
-}
-
-.in-stock-status {
-  font-size: 0.9em;
-  color: green;
-}
-
-.on-backorder-status {
-  font-size: 0.9em;
-  color: red;
-}
-
-.quantity-selector {
-  display: flex;
-  border: 1px solid #c1c1c1;
-  border-radius: 8px;
-  overflow: hidden;
-  /* Ensures the children do not break the rounded corners */
-}
-
-.quantity-change-button {
-  background-color: #ffffff;
-  border: none;
-  padding: 10px 12px;
-  cursor: pointer;
-  font-size: 1rem;
-  transition: all 0.2s;
-}
-
-.quantity-input {
-  border: none;
-  text-align: center;
-  width: 40px;
-  padding: 10px;
-  font-size: 1rem;
-  transition: all 0.2s;
-}
-
-.quantity-input:focus {
-  outline: none;
-}
-
-.remove-item {
-  background: none;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  font-size: 1.2em;
-  margin-left: 20px;
-}
-
-.quantity-change-button:hover,
-.quantity-change-button:focus,
-.quantity-input:focus,
-.remove-item:hover,
-.remove-item:focus {
-  background-color: #f2f2f2;
-}
-
-/* Styles for the order summary */
-.toggle-details-button {
-  background-color: #f1f1f1;
-  color: #333;
-  padding: 10px 15px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 10px;
-}
-
-.hide-order-details {
-  display: none;
-  /* Hide details by default */
-}
-
-.summary-item {
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0;
-}
-
-.summary-total {
-  display: flex;
-  justify-content: space-between;
-  font-weight: bold;
-  padding: 10px 0;
-  border-top: 1px solid #e2e2e2;
-  margin-top: 10px;
-}
-
-.checkout-button {
-  background-color: #4f46e5;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  width: 100%;
-  cursor: pointer;
-  margin-top: 10px;
-  transition: all 0.2s;
-  /* smooth transition in and out */
-}
-
-.checkout-button:hover {
-  background-color: #4138d9;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
